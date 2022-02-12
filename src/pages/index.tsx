c08444c486d0b5bb191ff688cc128ddb53ec62b3
Daniel Zordan <daniel.zordan@meliuz.com.br>
2022-02-12 10:48:57 -0400

    first configurations

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next CurrencyMoney</title>
+      </Head>
+      <h1>
+        Hello World
+      </h1>
+    </>
+  )
+}
